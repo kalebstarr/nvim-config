a70d0a018df362e8d3d6c163c62d4a0509d551a7
Kaleb <kaleb.starr@proton.me>
2025-08-26 01:22:14 +0200

    Set indentation size

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 require("config.options")
 
 require("config.lazy")
