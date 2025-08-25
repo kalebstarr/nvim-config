a57b348f0e05c72de6156ca5e4be46aa0e73cebd
Kaleb <kaleb.starr@proton.me>
2025-08-26 01:38:40 +0200

    Move vim options to appropriate file

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-
 require("config.options")
 
 require("config.lazy")
-
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
