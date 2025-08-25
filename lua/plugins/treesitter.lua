988a8cf886ba22a1e3723ef214d1f56a36cd5e4f
Kaleb <kaleb.starr@proton.me>
2025-08-26 01:33:33 +0200

    Make configuration more uniform

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,17 +5,21 @@ return {
         branch = 'master',
         build = ':TSUpdate',
 
-        opts = {
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "python",
-                "cpp",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        },
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = {
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "python",
+                    "cpp",
+                },
+                highlight = { enable = true },
+                indent = { enable = true },
+   
+            })
+        end
     },
 }
