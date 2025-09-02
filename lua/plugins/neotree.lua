23617cc22c32a6393decf48f1296ce0df61aa120
Kaleb <kaleb.starr@proton.me>
2025-09-02 22:58:16 +0200

    Prevent neotree from automatically starting

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	lazy = false,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    cmd = { "Neotree" },
+    keys = {
+        { "<C-n>", ":Neotree filesystem toggle left<CR>", desc = "Neo-tree" },
+    },
 
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
-	end,
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = { enabled = false },
+                hijack_netrw_behavior = "open_default",
+            },
+        })
+    end,
 }
