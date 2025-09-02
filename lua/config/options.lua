58e1e282f50f6a5d9e5d2baa763f2649e509bac5
Kaleb <kaleb.starr@proton.me>
2025-09-02 16:18:15 +0200

    Add inline code diagnostics

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,21 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
+
+vim.diagnostic.config({
+	virtual_text = { prefix = "●", spacing = 2 },
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = "if_many",
+		header = "",
+		prefix = "",
+	},
+})
+
+vim.keymap.set("n", "<leader>e", function()
+	vim.diagnostic.open_float(nil, { focus = false, scope = "line" })
+end, { desc = "Line diagnostics" })
